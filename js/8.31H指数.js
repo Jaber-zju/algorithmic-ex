a4d9b65505d8a27c125bcfe652dbe37c5f50c408
Jaber-zju <54564463+jaber-zju@users.noreply.github.com>
2020-08-31 22:23:13 +0800

    H 指数
    Signed-off-by: Jaber-zju <21825240@zju.edu.cn>

---
+++ js/8.31H指数.js
@@ -0,0 +1,25 @@
+// H 指数
+// https://leetcode-cn.com/problems/h-index/
+
+
+/**
+ * @param {number[]} citations
+ * @return {number}
+ */
+var hIndex = function (citations) {
+  citations = citations.sort((a, b) => a - b)
+  let maxH = 0
+  for (let i = 0; i < citations.length; i++) {
+    let temp = 0
+    if (citations[i] <= citations.length - i) {
+      temp = citations[i]
+    } else {
+      temp = citations.length - i
+    }
+    if (maxH < temp) maxH = temp
+  }
+  return maxH
+}
+
+const citations = [3, 0, 6, 1, 5]
+console.log(hIndex(citations))
