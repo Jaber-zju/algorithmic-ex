ba7925072f1e032b2d2bf299d55ee5f42af3b56a
Jaber-zju <21825240@zju.edu.cn>
2020-09-02 14:24:39 +0800

    二分查找排序
    Signed-off-by: Jaber-zju <21825240@zju.edu.cn>

--- js/9.02二分查找排序.js
+++ js/9.02二分查找排序.js
@@ -4,10 +4,10 @@ var erfen = function (val, arr) {
   if (arr.length < 1 || val < arr[0] || val > arr[arr.length - 1]) {
     return false;
   }//如果这个数字没在其中直接返回false
-  else if (val == arr[0] || val == arr[arr.length - 1]) {
+  else if (val === arr[0] || val === arr[arr.length - 1]) {
     return true;
   }//如果找到了就返回true
-  else if (arr.length == 1) {
+  else if (arr.length === 1) {
     return false;
   }//如果不能再缩小了而且没查到返回false
   var res = [];
@@ -15,7 +15,7 @@ var erfen = function (val, arr) {
   if (val > arr[base]) {
     res = arr.splice(base + 1, arr.length - 1);
   }//如果大于中间的从右边开始找
-  else if (val = arr[base]) {
+  else if (val === arr[base]) {
     return true;
   }//恰巧等于中间的就返回true
   else {
