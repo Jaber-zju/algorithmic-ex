9ccab665b701bdfda65cfcbec5fd7cdbffab5a1f
Jaber-zju <54564463+jaber-zju@users.noreply.github.com>
2020-09-11 19:45:40 +0800

    有效的完全平方数
    Signed-off-by: Jaber-zju <21825240@zju.edu.cn>

---
+++ js/9.11有效的完全平方数.js
@@ -0,0 +1,39 @@
+// 有效的完全平方数
+// https://leetcode-cn.com/problems/valid-perfect-square/
+
+
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+let isPerfectSquare = function (num) {
+  if (!num || num === 1) return true
+  else if (num === 2 || num === 3) return false
+  for (let i = 1; i <= Math.floor(num / 2); i++) {
+    if (i * i === num) return true
+  }
+  return false
+};
+
+
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+let isPerfectSquare = function (num) {
+  let low = 0;
+  let high = num;
+  let tmpSquare = 0;
+  while (low <= high) {
+    let mid = (low + high) >> 1;
+    tmpSquare = mid * mid;
+    if (tmpSquare > num) {
+      high = mid - 1;
+    } else if (tmpSquare < num) {
+      low = mid + 1;
+    } else {
+      return true;
+    }
+  }
+  return false;
+};
